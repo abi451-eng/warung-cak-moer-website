e348adee7c1b5929b1dafcd4a6a686c464701ca2
abi451-eng <bintangwahyu212@gmail.com>
2026-06-11 17:09:40 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    PENTING: setiap kali kamu mengubah index.html / file situs,
    naikkan nomor versi di bawah (mis. v1 -> v2) supaya pelanggan
    mendapat versi terbaru, bukan versi lama yang ter-cache. */
-const CACHE_VERSION = 'cakmoer-v5';
+const CACHE_VERSION = 'cakmoer-v6';
 const SHELL = [
   './',
   './index.html',
